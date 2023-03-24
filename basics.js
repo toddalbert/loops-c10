57ed9db56f5eb1336c0d9b8db1660c32cb27dbc0
Todd H. Albert <todd@bocacode.com>
2023-03-24 09:56:34 -0400

    did basic loops

--- basics.js
+++ basics.js
@@ -1,13 +1,15 @@
 // Write a loop to print "0. Hello" to "9. Hello" (10 lines)
 
-// write loop here... 
-
-
+for(let i = 0; i < 10; i++) {
+  console.log(`${i}. Hello`)
+}
 
 
 // Write a loop to count down from 10 to 1 (console.log)
 // and then write "Blast Off!"
 
-// write loop here... 
+for (let i = 10; i > 0; i--) {
+  console.log(i)
+}
 
-
+console.log("Blast Off!")
